8c2440e43331339278bcb87d872ef1e52a82131f
Gufron14 <gupron.nurjalil14@gmail>
2023-05-21 19:49:05 +0700

    aktif style

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,5 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-expand-sm navbar-dark bg-dark">
     <div class="container p-5">
+      <img src="{{ asset ('assets/img/logo.png') }}" alt="" style="width: 8%">
       <a class="navbar-brand  text-white fw-bold" href="#">Job Calling</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,16 +11,16 @@
                 <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
                     <a href="/" class="nav-link">Home</a>
                 </li>
-                <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+                <li class="nav-item {{ Request::path() == 'mitra' ? 'active' : '' }}">
                     <a href="mitra" class="nav-link">Mitra</a>
                 </li>
-                <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+                <li class="nav-item {{ Request::path() == 'proses' ? 'active' : '' }}">
                     <a href="proses" class="nav-link">Proses</a>
                 </li>
-                <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+                <li class="nav-item {{ Request::path() == 'riwayat' ? 'active' : '' }}">
                     <a href="riwayat" class="nav-link">Riwayat</a>
                 </li>
-                <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+                <li class="nav-item {{ Request::path() == 'gabungmitra' ? 'active' : '' }}">
                     <a href="gabungmitra" class="nav-link">Gabung Mitra</a>
                 </li>
                 <li class="nav-item dropdown">
@@ -39,3 +40,5 @@
       </div>
     </div>
   </nav>
+
+  

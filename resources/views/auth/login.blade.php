8eb8fc0bcd2baa92a6a5c1f309292431f5001117
Gufron14 <gupron.nurjalil14@gmail>
2023-06-07 03:26:32 +0700

    ganti model gambar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,40 +4,8 @@
     <div class="container p-5">
         <div class="row">
             <div class="col">
-              <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="{{ asset('assets/img/call1.jpg') }}" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>Panggil bantuan dimanapun kamu berada</h5>
-                    </div>
-                  </div>
-                  <div class="carousel-item" data-bs-interval="2000">
-                    <img src="{{ asset('assets/img/astronot.jpg') }}" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>Kalo lagi di Luar Angkasa please ga usah manggil</h5>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <img src="{{ asset('assets/img/call2.gif') }}" class="d-block w-100" alt="...">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>Apalagi kalo manggil cuma buat menghoetang</h5>
-                    </div>
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
+              <div class="d-flex justify-content-center align-items-center mt-5">
+                <img src="{{ asset ('assets/img/cs.png') }}" alt="" style="width: 60%">
               </div>
             </div>
             <div class="col m-5">
@@ -48,11 +16,11 @@
                             <h2 class="mb-3 text-center fw-bold">Login</h2>
                             <div class="mb-3">
                               <label for="exampleInputEmail1" class="form-label">Email address</label>
-                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required>
                             </div>
                             <div class="mb-3">
                               <label for="exampleInputPassword1" class="form-label">Password</label>
-                              <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                              <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
                             </div>
                             <div class="mb-3 text-danger">
                               {{$message}}

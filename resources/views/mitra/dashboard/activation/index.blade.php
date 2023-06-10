80b9c0b1f7418e8288a91ebfdc3c06cca29ac657
Gufron14 <gupron.nurjalil14@gmail>
2023-06-11 02:36:19 +0700

    ui view aktivasi

--- resources/views/mitra/dashboard/activation/index.blade.php
+++ resources/views/mitra/dashboard/activation/index.blade.php
@@ -1,5 +1,5 @@
-@extends('mitra.dashboard.layouts.main')
-@section('title', 'Customer')
+ @extends('mitra.dashboard.layouts.main')
+@section('title', 'Aktivasi Mitra')
 @section('content')
     <div class="container-fluid">
 
@@ -8,13 +8,20 @@
 
         <!-- DataTales Example -->
         <div class="card border-0 shadow mb-4">
-            <div class="card-header">
+            <div class="card-header d-flex justify-content-beetwen">
                 <p class="mb-0">Aktivasi Akun Mitra</p>
+                <p class="ml-auto">Status Mitra : <span> 
+                        @if ($partner['account_status'] == 0)
+                            <span>dalam proses aktivasi</span>
+                        @else
+                            <span>Aktif</span>
+                        @endif
+                    </span></p>
             </div>
             <div class="card-body d-inline-flex">   
                 {{-- Paket 1 Bulan --}}
                     <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card border-left-primary shadow h-100 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
@@ -30,7 +37,7 @@
 
                 {{-- Paket 6 Bulan --}}
                     <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card border-left-warning shadow h-100 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
@@ -59,92 +66,67 @@
                         </div>
                     </div> 
                  {{-- End  --}}
+                  
             </div>
+            <div class="ms-5 mb-5">
+                @if ($partner['account_status'] == 0)
+                    <i>* Lakukan Pembayaran untuk mengaktifkan</i>
+                @else
+                    
+                @endif
+              </div>
+        </div>
 
-            <div class="container">
-                <div class="mt-3">
-                    <p class="mb-3 text-danger">Mohon lakukan pembayaran untuk aktivasi Akun Mitra!</p>
-                    <p>Formulir Pembayaran</p>
-                    <form action="">
-                        <label for="partnername">Nama Mitra</label>
-                        <input class="form-control mb-3" type="text" value="{{ $partner['partner_name'] }}" aria-label="Disabled input example" disabled readonly>
-                        <div class="form-group">
-                            <select name="" id="selectOption" class="form-select" required>
-                                <option value="">-- Pilih Paket --</option>
-                                <option value="1 Bulan">1 Bulan</option>
-                                <option value="6 Bulan">6 Bulan</option>
-                                <option value="12 Bulan">12 Bulan</option>
-                            </select>
-                            <div class="invalid-feedback">Mohon pilih salah satu Paket</div>
-                        </div>
-                        <div class="mb-5 mt-5">
-                            <button type="submit" class="btn btn-primary" onclick="showModal()">Kirim</button>
-                        </div>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Pembayaran</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <table class="table table-bordered">
-                                        <thead class="bg-light">
-                                            <th>Bank</th>
-                                            <th>Nomor Rekening</th>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>BRI</td>
-                                                <td>1234567890981272</td>
-                                            </tr>
-                                            <tr>
-                                                <td>BCA</td>
-                                                <td>84360378467943876</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Dana</td>
-                                                <td>0899328457823</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Shoppepay</td>
-                                                <td>1234567890981272</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <div class="mt-5">
-                                        <label for="bukti" class="form-label">Upload Bukti Pembayaran</label>
-                                        <input type="file" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                <button type="submit" class="btn btn-primary"">Kirim</button>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-
-                    </form>
-
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Pembayaran Aktivasi</h1> 
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                
+                <div class="modal-body">
+                    <label for="" class="form-label">Bayar Melalui:</label>
+                    <table class="table table-bordered">
+                        <thead>
+                            <th>Nama Bank</th>
+                            <th>Nomor Rekening</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>BRI</td>
+                                <td>0987654321234</td>
+                            </tr>
+                            <tr>
+                                <td>BCA</td>
+                                <td>0987654321234</td>
+                            </tr>
+                            <tr>
+                                <td>ShopeePay</td>
+                                <td>081234567890</td>
+                            </tr>
+                            <tr>
+                                <td>Dana</td>
+                                <td>081234567890</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            <form action="" method="POST">
+                @csrf
+                    <div>
+                        <label for="bukti" class="form-label">Upload Bukti Pembayaran</label>
+                        <input type="file" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary">Kirim</button>
+                </div>
+            </form>
+            </div>
             </div>
         </div>
+        {{-- End Modal --}}
     </div>
-
-    <script>
-        function showModal() {
-            var selectOption = document.getElementById('selectOption');
-            if (!selectOption.value) {
-                selectOption.classList.add('is-invalid');
-            } else {
-                selectOption.classList.remove('is-invalid');
-                $('#myModal').modal('show');
-            }
-        }
-    </script>
 
 @endsection

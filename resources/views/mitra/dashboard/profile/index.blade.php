9b6308d4e5da7e243575e287273c5dae0c456113
Gufron14 <gupron.nurjalil14@gmail>
2023-06-11 04:28:44 +0700

    edit ui partner profile

--- resources/views/mitra/dashboard/profile/index.blade.php
+++ resources/views/mitra/dashboard/profile/index.blade.php
@@ -2,62 +2,54 @@
 @section('title', 'Partner')
 @section('content')
     <div class="container px-3">
-        <div class="card border-0 shadow mb-4">
-            <div class="card-header">
-                <p class="mb-0">Profil Mitra</p>
-            </div>
-            <div class="card-body">
-                <form action="">
-                    <div class="col-12 d-inline-flex mb-3">
-                        {{--Banner  --}}
-                        <div class="col-6 mb-3">
-                            <p>Banner</p>
-                            <input type="file" class="form-control">
-                        </div>
-                        {{-- End Banner --}}
-
-                        {{-- Feed --}}
-                        <div class="col-6 mb-3">
-                            <p>Feed</p>
-                            <input type="file" name="" id="" class="form-control">
-                        </div>
-                        {{-- End Feed --}}
+        <form action=" {{ url('/mitra.dashboard.profile.index') }} " method="POST" enctype="multipart/form-data">
+        @csrf
+            <div class="card border-0 shadow mb-4">
+                <div class="card-header  d-flex align-items-center justify-content-between">
+                    <p class="mb-0">Profil Mitra</p>
+                    <div class="float-end me-5">
+                        <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
                     </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 d-inline-flex mb-3">
+                            {{--Banner  --}}
+                            <div class="col-6 mb-3">
+                                <label for="" class="form-label">Banner</label>
+                                <input type="file" class="form-control">
+                            </div>
+                            {{-- End Banner --}}
 
-            {{-- PROFIL --}}
-                    <h5 class="mb-3 ms-3">Profil</h5>
-                    <div class="col-12 d-inline-flex">
-                        {{-- Tentang  --}}
-                        <div class="col-6 mb-3">
-                            <label for="" class="form-label">Nama Perusahaan</label>
-                            <input type="text" name="" id="" class="form-control">
+                            {{-- Feed --}}
+                            <div class="col-6 mb-3">
+                                <label for="" class="form-label">Feed</label>
+                                <input type="file" name="" id="" class="form-control">
+                            </div>
+                            {{-- End Feed --}}
                         </div>
-                        <div class="col-6 mb-3">
-                            <label for="" class="form-label">Alamat</label>
-                            <input type="text" name="" id="" class="form-control">
-                        </div>
-                        
-                        {{-- End Tentang --}}
-
-                        {{-- Layanan --}}
-                        
-                        {{-- End Layanan --}}
-
-                        {{-- Kontak --}}
-
-                        {{-- End Kontak --}}
                     </div>
-                    <div class="col-6 mb-3 p-3">
-                            <label for="" class="form-label">Deskripsi</label>
+                    <div class="row">
+                        <label for="" class="form-label h5 ms-3 mb-3">Profil</label>
+                        <div class="col-12 mb-3 d-inline-flex">
+                            <div class="col-6 mb-3">
+                                <label for="partner_name" class="form-label">Nama Perusahaan</label>
+                                <input type="text" id="partner_name" name="partner_name" class="form-control">
+                            </div>
+                            <div class="col-6 mb-3">
+                                <label for="address" class="form-label">Alamat Perusahaan</label>
+                                <input type="text" id="address" name="address" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-11 m-2">
+                            <label for="" class="form-label">Deskripsi Perusahaan</label>
                             <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
                         </div>
-            {{-- END PROFIL --}}
-
-                        <div class="float-end me-5 mb-3">
-                            <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
-                        </div>
-                </form>
-                
-        </div>
-    </script>
+                    </div>
+                </div>      
+            </div>
+        </form>
+    
 @endsection

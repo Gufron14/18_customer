2e8db3eb4b0d44dca15f4e96f36af8a597a932e1
Gufron14 <gupron.nurjalil14@gmail>
2023-06-11 04:29:44 +0700

    toggle switch on progress

--- resources/views/mitra/dashboard/index.blade.php
+++ resources/views/mitra/dashboard/index.blade.php
@@ -7,8 +7,10 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+        <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+            <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+          </div>
     </div>
 
     <!-- Content Row -->

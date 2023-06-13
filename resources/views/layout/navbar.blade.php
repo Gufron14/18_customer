b1f85e62b8271bd4ccc0adcd12d6e2afc166cedd
Gufron14 <gupron.nurjalil14@gmail>
2023-06-13 22:14:32 +0700

    ui login

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -16,44 +16,44 @@
   
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container p-5">
-      <img src="{{ asset ('assets/img/logo.png') }}" alt="" style="width: 8%">
-      <a class="navbar-brand  text-white fw-bold" href="#">Job Calling</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
-                    <a href="/" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item {{ Request::path() == 'mitra' ? 'active' : '' }}">
-                    <a href="mitra" class="nav-link">Mitra</a>
-                </li>
-                <li class="nav-item {{ Request::path() == 'proses' ? 'active' : '' }}">
-                    <a href="proses" class="nav-link">Proses</a>
-                </li>
-                <li class="nav-item {{ Request::path() == 'riwayat' ? 'active' : '' }}">
-                    <a href="riwayat" class="nav-link">Riwayat</a>
-                </li>
-                <li class="nav-item {{ Request::path() == 'gabungmitra' ? 'active' : '' }}">
-                  @if (session('role') == 0)
-                  <a href="gabungmitra" class="nav-link">Gabung Mitra</a>
 
-                    @else
-                        
-                    <a href="statusmitra" class="nav-link">Toko Saya</a>
-                    @endif
-                </li>
-
-              
-
-                {{-- Profile --}}
 
                 @if(session('token'))
-
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                  <div class="container p-5">
+                    <img src="{{ asset ('assets/img/logo.png') }}" alt="" style="width: 8%">
+                    <a class="navbar-brand  text-white fw-bold" href="#">Job Calling</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                      <div class="navbar-nav">
+                          <ul class="navbar-nav ml-auto">
+                              <li class="nav-item {{ Request::path() == '/' ? 'active' : '' }}">
+                                  <a href="/" class="nav-link">Home</a>
+                              </li>
+                              <li class="nav-item {{ Request::path() == 'mitra' ? 'active' : '' }}">
+                                  <a href="mitra" class="nav-link">Mitra</a>
+                              </li>
+                              <li class="nav-item {{ Request::path() == 'proses' ? 'active' : '' }}">
+                                  <a href="proses" class="nav-link">Proses</a>
+                              </li>
+                              <li class="nav-item {{ Request::path() == 'riwayat' ? 'active' : '' }}">
+                                  <a href="riwayat" class="nav-link">Riwayat</a>
+                              </li>
+                              <li class="nav-item {{ Request::path() == 'gabungmitra' ? 'active' : '' }}">
+                                @if (session('role') == 0)
+                                <a href="gabungmitra" class="nav-link">Gabung Mitra</a>
+              
+                                  @else
+                                      
+                                  <a href="statusmitra" class="nav-link">Toko Saya</a>
+                                  @endif
+                              </li>
+              
+                            
+              
+                              {{-- Profile --}}
                 {{-- NOTIFIKASI --}}
                 <div class="notif-bg d-flex align-items-center me-3">
                   <div class="container d-flex justify-content-center">

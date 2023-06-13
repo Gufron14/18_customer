78a2a1b012f8709e5a37c85322e59816b50bd37d
Gufron14 <gupron.nurjalil14@gmail>
2023-06-13 22:14:23 +0700

    ui proses

--- resources/views/proses.blade.php
+++ resources/views/proses.blade.php
@@ -4,85 +4,30 @@
   <div class="container p-5">
     <div class="row mb-3 m-3">
       @foreach($calls as $call)
-        <div class="card mb-3">
-          <div class="card-body d-flex flex-row">
-            <div class="col-4 d-flex d-inline-flex">
+        <div class="card mb-3 mx-auto">
+          <div class="card-body d-flex flex-row d-inline-flex">
+            <div class="col-3">
               <img src="http://localhost:5000/api/user/partner/avatar/{{ $call['partner_id'] }}?token={{session('token')}}" alt="" style="width: 200px; height:200px;">
             </div>
-            <div class="col-4 d-inline-flex d-flex flex-column ms-3 mt-3">
-              <div class="d-flex">{{$call['partner']['partner_name']}}</div>
-              {{-- <h4 class="fw-bold d-block-flex">{{ $call['problem'] }}</h4> --}}
+            <div class="col-6 me-5 d-flex flex-column justify-content-center">
+              <span>anda memanggil</span>
+              <div class="d-flex h4 fw-bold">{{$call['partner']['partner_name']}}</div>
+              {{-- <h4 class="fw-bold">Ini Judul Masalahnya</h4> --}}
               <div class="d-flex">
                 {{ $call['message'] }}
               </div>
             </div>
-            <div class="col-6 mt-3">
-              <h6 id="text">{{$call['progres']['progres']}}
-              </h6>
-              <h1 id="timer" class="fw-bold"></h1>
-              <button class="btn btn-danger mt-3" id="cancelBtn">
+            <div class="col-2 d-flex flex-column justify-content-center align-items-center">
+              <p>{{ $call['updated_at'] }}</p>
+              <h6>{{$call['progres']['progres']}}</h6>
+              <button class="btn btn-danger mt-3" {{ $call['order_status'] !== 1 && $call['order_status'] !== 2 ? 'disabled' : '' }}>
                 Batalkan Panggilan
               </button>
-              </div>
             </div>
           </div>
       </div>
       @endforeach
     </div>
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script>
-    var countdownTime = 300;
-    var timerInterval;
 
-    function formatTime(seconds) {
-      var minutes = Math.floor(seconds / 60);
-      var remainingSeconds = seconds % 60;
-
-      return `${padZero(minutes)}:${padZero(remainingSeconds)}`;
-    }
-
-    function padZero(number) {
-      return number < 10 ? '0' + number : number;
-    }
-
-    function updateTimer() {
-      var timerElement = document.getElementById('timer');
-
-      countdownTime--;
-      timerElement.innerText = formatTime(countdownTime);
-
-      if (countdownTime <= 0) {
-        clearInterval(timerInterval);
-        timerElement.innerText = 'Waktu habis!';
-        document.getElementById('cancelBtn').disabled = true;
-      }
-    }
-
-    function cancelTimer() {
-      clearInterval(timerInterval);
-      document.getElementById('cancelBtn').innerText = 'Panggilan Dibatalkan';
-      document.getElementById('cancelBtn').disabled = true;
-      document.getElementById('text').innerText = 'Panggilan Dibatalkan';
-    }
-
-    function startTimer() {
-      timerInterval = setInterval(updateTimer, 1000);
-      document.getElementById('cancelBtn').removeAttribute('disabled');
-    }
-
-    // Cek status order dan atur tombol
-    var partnerOrderStatus = <?php echo $call['order_status']; ?>;
-    if (partnerOrderStatus !== 0) {
-      document.getElementById('cancelBtn').disabled = true;
-    } else {
-      startTimer();
-    }
-
-    // Tambahkan event listener pada tombol "Batalkan Panggilan"
-    document.getElementById('cancelBtn').addEventListener('click', function() {
-      cancelTimer();
-      countdownTime = 0;
-    });
-  </script>
 @endsection
